c66ac6e80ac057e20b720dc8dfa4d274a0b23fdd
sandhya <sandhya@LAPTOP-0NHJCJJ3>
2026-02-07 00:22:38 +0530

    Day1

---
+++ TNSDailyCode_1/src/CharDemo.java
@@ -0,0 +1,18 @@
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        char ch='a';
+        System.out.println(ch);
+
+        char ch1=65;
+        System.out.println(ch1);
+
+        int ch2='A';
+        System.out.println(ch2);
+
+	}
+
+}
